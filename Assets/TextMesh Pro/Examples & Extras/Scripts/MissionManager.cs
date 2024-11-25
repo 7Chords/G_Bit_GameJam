4f60a3adfe5745ad29d942ebbcd32adfe82678e1
DAIGOBLAHBLAHBLAH <18929310512@163.com>
2024-11-25 16:56:51 +0800

    面板修改

--- Assets/TextMesh Pro/Examples & Extras/Scripts/MissionManager.cs
+++ Assets/TextMesh Pro/Examples & Extras/Scripts/MissionManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MissionManager : MonoBehaviour
+public class MissionManager : BasePanel
 {
     
     public CurrentMissionSO CurrentMission;
