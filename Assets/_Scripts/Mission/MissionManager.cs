c778dc7df70e5b13b2136397b0d31e267497035c
小桂花 <143058334+guihuala@users.noreply.github.com>
2024-11-25 22:32:41 +0800

    滑梯瓦片

--- Assets/_Scripts/Mission/MissionManager.cs
+++ Assets/_Scripts/Mission/MissionManager.cs
@@ -5,29 +5,24 @@ using UnityEngine.UI;
 
 public class MissionManager : BasePanel
 {
-    
     public CurrentMissionSO CurrentMission;
     public MissionPanelSO MissionList;
     int id;
-
-
+    
     [SerializeField]
     Text MissionName,ClientName, MissionTime, MissionDetail;
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         copyMission(MissionList, CurrentMission, id);
         MissionDisplay(CurrentMission);
     }
+    
     public MissionInformation GetMissionInformation(int id)
     {
         return MissionList.MissionList.Find(i=>i.MissionId == id);
     }
+    
     void copyMission(MissionPanelSO sourse,CurrentMissionSO currentMission, int id)
     {
         MissionInformation copyMission = sourse.MissionList.Find(mission=>mission.MissionId ==id);
@@ -44,6 +39,7 @@ public class MissionManager : BasePanel
             currentMission.clearData();
         }
     }
+    
     void MissionDisplay(CurrentMissionSO currentMission)
     {
         if (currentMission != null)
@@ -54,6 +50,7 @@ public class MissionManager : BasePanel
             MissionDetail.text = currentMission.MissionDetail;
         }
     }
+    
     public void OnButtonClick(string arg)
     {
         id = int.Parse(arg);
