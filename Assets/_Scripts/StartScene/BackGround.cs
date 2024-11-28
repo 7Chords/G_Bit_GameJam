61613724f0b04c43b56476b1bd114685d29eec3b
kulou0cat <1056203245@qq.com>
2024-11-28 23:45:03 +0800

    开始界面

---
+++ Assets/_Scripts/StartScene/BackGround.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using UnityEngine;
+
+//±³¾°ÊÓ²îÐ§¹û
+public class BackGround : MonoBehaviour
+{
+    public float offsetMultiplier = 1f;
+    public float smoothTime = .3f;
+
+    private Vector2 startPosition;
+    private Vector3 velocity;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        Vector2 offset=Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        Vector3 targetPosition = startPosition + (offset * offsetMultiplier);
+        transform.position=Vector3.SmoothDamp(transform.position,startPosition+(offset*offsetMultiplier),ref velocity,smoothTime);
+    }
+}
